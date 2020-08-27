7b44281aa2aaf43c7c96f11a6721b9ffb289eb9c
Michael Zhou <zhoumichael8@gmail.com>
2020-08-27 11:41:05 +0100

    Add styling and animation to Nav component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Nav from './Nav';
 
 function App() {
   return (
     <div className="app">
       <Header/>
 
-      {/* Nav */}
+      <Nav />
 
       {/* Results */}
     </div>
